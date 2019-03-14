7c45b6f29b8360b24eb4b9f9ee6f62a6970b6936
Thomas Brasington <tom@2c2d.co.uk>
2019-03-14 07:20:45 +0000

    new preview rendering

--- src/renderStyles.js
+++ src/renderStyles.js
@@ -1,8 +1,4 @@
-import React from 'react'
-
-// import {render, TextStyles} from 'react-sketchapp'
-//import {TypeLayout} from './typeSheet';
-import  sketch from 'sketch/dom'
+import  sketch, {Text} from 'sketch/dom'
 
 import {extractStyles,generateTextStyles} from './generators'
 
@@ -13,23 +9,36 @@ export default function(context) {
   const designTokens = extractStyles(context,false)
   const textStyles = generateTextStyles(designTokens);
 
+  let RenderPage =  context.document.addBlankPage()
+  RenderPage.name="Rendered Styles";
 
   let document = sketch.fromNative(context.document)
-  document.sharedTextStyles ={}
-  Object.keys(textStyles).forEach(style=>{
-  //  console.log(textStyles[style])
 
+
+  document.sharedTextStyles ={}
+  let count=0, previousFrame = null;
+  Object.keys(textStyles).forEach(style=>{
+    
     document.sharedTextStyles.push({
         name: String(style),
         style: textStyles[style]
-      })
+      });
+
+      console.log ( (previousFrame !=null ? previousFrame.fra : 0) )
+      let textLayer = new Text({
+        text: String(style),
+        parent : RenderPage,
+        style: textStyles[style],
+        frame : { x : 0 , y :  (previousFrame   !=null ? Math.ceil(previousFrame.frame.height + previousFrame.frame.y + 24) : 0)}
+      });
+
+      previousFrame = textLayer;
+      count++;
   })
 
   // success message
   context.document.showMessage(`${Object.keys(textStyles).length} styles added (${Object.keys(designTokens.typography).length} Text Styles * ${Object.keys(designTokens.colours).length} colours * ${Object.keys(designTokens.textAlignments).length} alignments) 🙌`);
 
-// let RenderPage =  context.document.addBlankPage()
-// RenderPage.name="Rendered Styles"
 // render(<TypeLayout colours={designTokens.colours}  typography={designTokens.typography}/>, RenderPage);
 
 }
